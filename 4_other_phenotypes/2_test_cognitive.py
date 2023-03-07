2a8cd9e7ab39a3b68ed14928a0f740bcb4f957ad
Corrine Smolen <ces6136@qingyu.bx.psu.edu>
2023-03-07 17:36:42 -0500

    cognition_update

--- 4_other_phenotypes/2_test_cognitive.py
+++ 4_other_phenotypes/2_test_cognitive.py
@@ -74,4 +74,48 @@ for p in phenos:
 	skew_dat.append([p, sk, sk2, kurt, kurt2])
 pdf.close()
 skew_df=pd.DataFrame(skew_dat, columns=['Phenotype', 'Skew', 'Skew_after_norm', 'Kurtosis', 'Kurtosis_after_norm'])
-print(skew_df)
+# Save skew data
+skew_df.to_csv('result_tables/2_cognition_skew.csv', index=False)
+
+
+# Convert to Z score
+score_cols=['pairs_matching_errors+norm','reaction_time_mean+norm', 'fluid_intelligence', 'numeric_memory_maxdigits', 'sd_sub_correctmatches+norm']
+for sc in score_cols:
+	name=sc.split('+')[0]
+
+	vals=df[~df[sc].isnull()][sc].to_numpy()
+
+	score_mean=np.mean(vals)
+	score_sd=np.std(vals)
+
+	df[name+'_Z']=(df[sc] - score_mean)/score_sd
+
+	# Change direction of scores so that lower score is worse and higher score is better
+	if sc in ['pairs_matching_errors+norm','reaction_time_mean+norm']:
+		df[name+'_Z']=df[name+'_Z']*-1
+
+
+# Perform t-tests and plot distributions
+z_cols = [i for i in df.columns.to_list() if 'Z' in i]
+controls=['NoCNV_Control', 'LargeRare_Control', 'NEJM_Control']
+stat_lst=[]
+pdf=PdfPages('Figures/2_coginition_violins.pdf')
+for z in z_cols:
+	for c in controls:
+		case_vals=df[(df.Case_Control=='Case') & (~df[z].isnull())][z].to_numpy()
+		cont_vals=df[(df.Case_Control==c) & (~df[z].isnull())][z].to_numpy()
+
+		# T-test
+		t, p = stats.ttest_ind(case_vals, cont_vals, alternative='less')
+		stat_lst.append([z, c, 'one tailed t-test, less', np.nanmean(case_vals), np.nanmean(cont_vals), t, p])
+
+		# Violinplot
+		sns.violinplot(data=df[df.Case_Control.isin(['Case', c])], y=z, hue='Case_Control')
+		pdf.savefig()
+		plt.close()
+
+pdf.close()
+
+stat_df=pd.DataFrame(stat_lst, columns=['Measure', 'Control', 'Test', 'Case_mean', 'Control_mean', 'statistic', 'p-value'])
+print(stat_df)
+stat_df.to_csv('result_tables/2_cognition_ttest.csv', index=False)
